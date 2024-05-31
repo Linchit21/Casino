5ad584bf7dce3bf02e488e5c058f92a99c16733d
Linchit21 <wladev.it@gmail.com>
2024-05-31 20:31:41 +0200

    casino_work_v1

---
+++ dist/index.09c24910.js
@@ -0,0 +1,3 @@
+new Swiper(".swiper");
+
+//# sourceMappingURL=index.09c24910.js.map
---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+new Swiper('.swiper', {
+    navigation: {
+        nextEl: '.packets__blocks-direction_right',
+        prevEl: '.packets__blocks-direction',
+      },
+      loop: true,
+      slidesPerView: 4,
+      spaceBetween: 40,
+
+});
