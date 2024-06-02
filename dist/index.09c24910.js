d4275503ccd59ce2ef91270ca0bf981c4aac453e
Linchit21 <wladev.it@gmail.com>
2024-06-02 11:37:43 +0200

    fix_2.06.2024

--- dist/index.09c24910.js
+++ dist/index.09c24910.js
@@ -1,3 +1,11 @@
-new Swiper(".swiper");
+new Swiper(".swiper", {
+    navigation: {
+        nextEl: ".packets__blocks-direction_right",
+        prevEl: ".packets__blocks-direction"
+    },
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 40
+});
 
 //# sourceMappingURL=index.09c24910.js.map
---
+++ dist/index.705124a9.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{navigation:{nextEl:".packets__blocks-direction_right",prevEl:".packets__blocks-direction"},loop:!0,slidesPerView:4,spaceBetween:40});
+//# sourceMappingURL=index.705124a9.js.map
